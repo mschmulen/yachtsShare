bf5266aee100896d42cc83844b73199007a0f620
mschmulen <matt.schmulen@gmail.com>
2017-01-11 16:01:08 -0600

    user update

--- Sources/User.swift
+++ Sources/User.swift
@@ -5,6 +5,27 @@ public struct User: Identifiable {
   public let id:Identifier
   public let name:String
   public let email:String
+  public let imageURL:String
+
+  static public func deserialize(dictionary:Dictionary<String, Any>) -> User {
+    let model = User(
+      id: dictionary["id"] as! String,
+      name: dictionary["name"] as! String,
+      email: dictionary["email"] as! String,
+      imageURL: dictionary["imageURL"] as! String
+    )
+    return model
+  }
+
+  public func serialize() -> [String: Any] {
+    var model = [String: Any]()
+    model["id"] = self.id
+    model["name"] = self.name
+    model["email"] = self.email
+    model["imageURL"] = self.imageURL
+    return model
+  }
+
 }
 
 extension User: Equatable {
